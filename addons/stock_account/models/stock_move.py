edd0a65f92d10c1802cca5d73163b63fbfdbe8a7
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2023-03-24 18:25:27 +0100

    [FIX] stock_account: rounding of price digit should be using Product Price decimal precision

--- addons/stock_account/models/stock_move.py
+++ addons/stock_account/models/stock_move.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
-from odoo.tools import float_is_zero, OrderedSet
+from odoo.tools import float_round, float_is_zero, OrderedSet
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -41,7 +41,10 @@ class StockMove(models.Model):
         if self.origin_returned_move_id and self.origin_returned_move_id.sudo().stock_valuation_layer_ids:
             layers = self.origin_returned_move_id.sudo().stock_valuation_layer_ids
             quantity = sum(layers.mapped("quantity"))
-            return layers.currency_id.round(sum(layers.mapped("value")) / quantity) if not float_is_zero(quantity, precision_rounding=layers.uom_id.rounding) else 0
+            product_price_digits = self.env['decimal.precision'].precision_get('Product Price')
+            price_unit = float_round(sum(layers.mapped("value")) / quantity if not float_is_zero(
+                quantity, precision_rounding=layers.uom_id.rounding) else 0, precision_digits=product_price_digits)
+            return price_unit
         return price_unit if not float_is_zero(price_unit, precision) or self._should_force_price_unit() else self.product_id.standard_price
 
     @api.model
